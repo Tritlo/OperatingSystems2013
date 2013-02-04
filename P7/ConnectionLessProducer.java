80f85904db522fa450949cc74d00ce3d3c845a60
Matthias Pall Gissurarson <mpg@mpg.is>
2013-02-04 21:25:36 +0000

    Baetti vid java skram, vantadi i sidasta commit

---
+++ P7/ConnectionLessProducer.java
@@ -0,0 +1,42 @@
+package ch3Processes.connectionLessSocket;
+import java.io.*;
+import java.net.*;
+
+public class ConnectionLessProducer
+{
+  public static void main(String args[])
+  { // args[0]: message contents,
+    // args[1]: destination hostname
+    DatagramSocket aSocket = null;
+    try 
+    {
+      aSocket = new DatagramSocket();
+      String stringMsg = "0";
+      String prevReply = "0";
+      InetAddress aHost = InetAddress.getByName("localhost");
+      int serverPort = 6789; // agreed port
+      while(true)
+      {
+	     stringMsg = "" + (Integer.parseInt(stringMsg)+1);
+             byte[] message = stringMsg.getBytes();
+	     DatagramPacket request = new DatagramPacket(message, message.length,
+		      aHost, serverPort);
+	     aSocket.send(request);
+	     byte[] buffer = new byte[1000];
+	     DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+	     aSocket.receive(reply);
+	     System.out.println("Reply: " + new String(reply.getData()));
+      }
+    } catch (SocketException e)
+    {
+      System.out.println("Socket: " + e.getMessage());
+    } catch (IOException e) 
+    {
+      System.out.println("IO: " + e.getMessage());
+    } finally
+    {
+      if (aSocket != null)
+        aSocket.close();
+    }
+  }
+}
