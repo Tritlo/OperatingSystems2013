63444c57e982fdb78dbe2f7734aa4921d167db9d
Matthias Pall Gissuarson <mpg@mpg.is>
2013-02-27 15:19:02 +0000

    P14 done

--- P13-14/RaceThreadsSemaphore.java
+++ P13-14/RaceThreadsSemaphore.java
@@ -3,15 +3,22 @@ public class RaceThreadsSemaphore
 {
 	volatile int in;
 	public int COUNT_MAX;
-	private Semaphore sem = new Semaphore(1);
-	
+        private Semaphore sem = new Semaphore(1); 
+        //If we had 2 here, 2 threads would enter the critical section at the same time.
+    
 	public class CountThread extends Thread
 	{
+	    public CountThread(String name)
+	    {
+		    super(name);
+	    }
 		public void run()
 		{
 			for(int i = 0; i < COUNT_MAX; i++)
 			{
 			    sem.acquireUninterruptibly();
+			    System.out.print(this.getName());
+			    System.out.flush();
 			    in++;
 			    sem.release();
 			}
@@ -26,8 +33,8 @@ public class RaceThreadsSemaphore
 	public RaceThreadsSemaphore(int max)
 	{
 		this.COUNT_MAX = max;
-		Thread counter1 = new CountThread();
-		Thread counter2 = new CountThread();
+		Thread counter1 = new CountThread("0");
+		Thread counter2 = new CountThread("1");
 		this.in = 0;
 		
 		counter1.start();
@@ -38,7 +45,7 @@ public class RaceThreadsSemaphore
 			counter1.join();
 			counter2.join();
 		} catch (InterruptedException ie) {return;}
-		
+		System.out.println();
 		System.out.println("COUNT_MAX = " + max + "\nin = " + in + "\n2*COUNT_MAX - in = " + (2*max - in));
 	}
 }
